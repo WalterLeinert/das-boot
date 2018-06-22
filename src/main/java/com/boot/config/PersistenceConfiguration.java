ba85e8fb4a66a19814144e60e8ace61e65d981ff
Walter Leinert <walter.leinert@hp.com>
2018-06-22 10:56:17 +0200

    fixes for secondary datasource

--- src/main/java/com/boot/config/PersistenceConfiguration.java
+++ src/main/java/com/boot/config/PersistenceConfiguration.java
@@ -3,27 +3,46 @@ package com.boot.config;
 import javax.sql.DataSource;
 
 import org.springframework.boot.autoconfigure.flyway.FlywayDataSource;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceProperties;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.boot.jdbc.DataSourceBuilder;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Primary;
 
+/**
+ * @author Walter
+ *
+ */
 @Configuration
 public class PersistenceConfiguration {
+	
+	@Bean
+	@Primary
+	@ConfigurationProperties("spring.datasource")
+	public DataSourceProperties dataSourceProperties() {
+		return new DataSourceProperties();
+	}
 
 	@Bean
 	@ConfigurationProperties(prefix="spring.datasource")
 	@Primary
 	public DataSource dataSource() {
-		return DataSourceBuilder.create().build();
+		return dataSourceProperties().initializeDataSourceBuilder().build();
 	}
 	
+	
+	
+	@Bean
+	@ConfigurationProperties("datasource.flyway")
+	public DataSourceProperties flywayDataSourceProperties() {
+		return new DataSourceProperties();
+	}
 	
 	@Bean
 	@ConfigurationProperties(prefix="datasource.flyway")
 	@FlywayDataSource
 	public DataSource flywayDataSource() {
-		return DataSourceBuilder.create().build();
+		return flywayDataSourceProperties().initializeDataSourceBuilder().build();
 	}
 }
